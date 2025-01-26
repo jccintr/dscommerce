591dbb516d41ee328e287a03c09dfec62f8b6dab
Julio Cesar <jccintr@gmail.com>
2025-01-25 23:49:08 -0300

    Mapeado a entitade User e configurado o H2

--- src/main/java/com/jcsoftware/dscommerce/entities/User.java
+++ src/main/java/com/jcsoftware/dscommerce/entities/User.java
@@ -2,17 +2,27 @@ package com.jcsoftware.dscommerce.entities;
 
 import java.time.LocalDate;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
 public class User {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
 	private String phone;
 	private LocalDate birthDate;
 	private String password;
-	
+
 	public User() {
-		
+
 	}
 
 	public User(Long id, String name, String email, String phone, LocalDate birthDate, String password) {
@@ -72,6 +82,5 @@ public class User {
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	
-	
+
 }
