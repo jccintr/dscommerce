52f4f95aa0c5887d7c28f260a49bd2ad204ef397
Julio Cesar <jccintr@gmail.com>
2025-01-28 15:12:27 -0300

    Implementado inserção de produtos

--- src/main/java/com/jcsoftware/dscommerce/controllers/ProductController.java
+++ src/main/java/com/jcsoftware/dscommerce/controllers/ProductController.java
@@ -5,12 +5,14 @@ package com.jcsoftware.dscommerce.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.jcsoftware.dscommerce.dtos.ProductDTO;
 import com.jcsoftware.dscommerce.services.ProductService;
 
@@ -33,6 +35,12 @@ public class ProductController {
 	public ResponseEntity<ProductDTO> findById(@PathVariable Long id){
 		ProductDTO productDTO = service.findById(id);
 		return ResponseEntity.ok().body(productDTO);
+	}
+	
+	@PostMapping
+	public ResponseEntity<ProductDTO> insert(@RequestBody ProductDTO dto){
+		ProductDTO newProductDTO = service.insert(dto);
+		return ResponseEntity.status(HttpStatus.CREATED).body(newProductDTO);
 	}
 
 }
--- src/main/java/com/jcsoftware/dscommerce/services/ProductService.java
+++ src/main/java/com/jcsoftware/dscommerce/services/ProductService.java
@@ -1,8 +1,8 @@
 package com.jcsoftware.dscommerce.services;
 
 
-import java.util.List;
 import java.util.Optional;
+
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +10,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import com.jcsoftware.dscommerce.dtos.ProductDTO;
 import com.jcsoftware.dscommerce.entities.Product;
 import com.jcsoftware.dscommerce.repositories.ProductRepository;
@@ -20,33 +19,40 @@ public class ProductService {
 
 	@Autowired
 	private ProductRepository repository;
-	
 
 	public Page<ProductDTO> findAll(Pageable pageable) {
 
 		Page<Product> products = repository.findAll(pageable);
- 		//List<ProductDTO> dtos = new ArrayList<>();
-		return products.map(x-> new ProductDTO(x));
-		
+		// List<ProductDTO> dtos = new ArrayList<>();
+		return products.map(x -> new ProductDTO(x));
+
 		/*
- 		for(Product p: products) {
- 			ProductDTO dto = new ProductDTO();
- 			BeanUtils.copyProperties(p, dto);
- 			dtos.add(dto);
- 		}
- 		return dtos; 
-*/
+		 * for(Product p: products) { ProductDTO dto = new ProductDTO();
+		 * BeanUtils.copyProperties(p, dto); dtos.add(dto); } return dtos;
+		 */
 	}
 
-	@Transactional(readOnly=true)
+	@Transactional(readOnly = true)
 	public ProductDTO findById(Long id) {
 
 		Optional<Product> productO = repository.findById(id);
-		Product product =  productO.get();
+		Product product = productO.get();
 		ProductDTO dto = new ProductDTO();
 		BeanUtils.copyProperties(product, dto);
 		return dto;
+
+	}
+	
+	@Transactional
+	public ProductDTO insert(ProductDTO dto) {
+
+		Product newProduct = new Product();
+		BeanUtils.copyProperties(dto, newProduct);
+		newProduct = repository.save(newProduct);
 		
+		//ProductDTO newProductDTO = new ProductDTO();
+	//	BeanUtils.copyProperties(productCreated, newProductDTO);
+		return new ProductDTO(newProduct);
 	}
 
 }
